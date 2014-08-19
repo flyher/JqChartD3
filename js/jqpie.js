3bd021fcab8000b593cbb047e8cdc263dabf1d06
flyher <flyher.dong@meihua.info>
2014-08-19 18:05:43 +0800

    fix html templat

--- js/jqpie.js
+++ js/jqpie.js
@@ -7,20 +7,30 @@ $(document).ready(function () {
     for (var i = 0; i < json.length; i++) {
         data[i] = JSON.parse(json[i]);
     }
-    DrawPie("chart", data, true, 460, 320);
+    DrawPie("chart", data, true, 460, 320,0,0);
 });
 
-function DrawPie(divid, data, tips, width, height) {
+function DrawPie(divid, data, tips, width, height, outWidth, innerWidth) {
     ///<summary>绘制饼状图</summary>
     ///<param name="divid" type="String">div的id</param>
     ///<param name="data" type="Array">已经转换为对象数组的json数据</param>
     ///<param name="tips" type="Bool">是否显示扇形标记</param>
     ///<param name="width" type="Inter">宽度</param>
     ///<param name="height" type="Inter">高度</param>
+    ///<param name="outWidth" type="Inter">外圈大小，设置默认为0</param>
+    ///<param name="innerWidth" type="Inter">内圈大小，设置为0时默认为height的1/2</param>
     //var width = 960,
     //height = 600,
     //外圈
-    var radiusout = height / 2;
+    //var radiusout = height / 2;
+
+    if (height <= innerWidth) {
+        console.error("宽度设置不正确");
+    }
+
+    if (innerWidth == 0) {
+        innerWidth = height / 2;
+    }
 
     var color = d3.scale.ordinal()//构造一个ordinal变换对象
         .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
@@ -30,8 +40,10 @@ function DrawPie(divid, data, tips, width, height) {
         .sort(null); //设置饼图的顺时针方法的排序方法
 
     var arc = d3.svg.arc()//创建弧发生器
-        .outerRadius(0)//外圈
-        .innerRadius(radiusout); //内圈
+        .outerRadius(outWidth)//外圈
+        .innerRadius(innerWidth); //内圈
+    //.outerRadius(0)//外圈
+    //.innerRadius(radiusout); //内圈
 
     var svg = d3.select("#" + divid).append("svg")
         .attr("width", width)
@@ -85,7 +97,6 @@ function DrawPie(divid, data, tips, width, height) {
             } else {
                 return "";
             }
-
         });
 }
 
